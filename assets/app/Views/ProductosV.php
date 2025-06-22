813858c9759a4ea0a48bf13e0970646c722ca54f
edwin1327 <edwinfabian.13@gmail.com>
2025-06-21 20:43:42 -0500

    Elaborando estilos a la página de productos

--- assets/app/Views/ProductosV.php
+++ assets/app/Views/ProductosV.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/productos.css">
     <title>Inventario</title>
 </head>
 <body>
@@ -17,14 +18,15 @@
             </nav>
             <div class="table">
                 <div class="busqueda">
-                    <a href="Busqueda.php"><button class="busq">Editar Producto</button></a>
+                    <a href="Busqueda.php" class="link"><button class="busq">Editar Producto</button></a>
                 </div>
+                <h1>Productos en el inventario</h1>
                 <table>
                     <thead>
                         <tr>
-                            <td>Nombre</td>
-                            <td>descripción</td>
-                            <td>Precio</td>
+                            <td class="title">Nombre</td>
+                            <td class="title">Descripción</td>
+                            <td class="title">Precio</td>
                         </tr>
                     </thead>
                     <tbody>
